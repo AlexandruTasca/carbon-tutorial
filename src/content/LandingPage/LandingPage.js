627efaadd4749d6b6c0655158752c6c0541f967f
Alexandru Tasca <BEX1110@be.ibm.com>
2021-02-16 11:32:29 +0100

    feat(tutorial): complete step 1

---
+++ src/content/LandingPage/LandingPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return <div>LANDING PAGE for Carbon Tutorial with React</div>;
+};
+
+export default LandingPage;
---
+++ src/content/RepoPage/RepoPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RepoPage = () => {
+  return <div>REPO PAGE as taken from Carbon tutorial</div>;
+};
+
+export default RepoPage;
